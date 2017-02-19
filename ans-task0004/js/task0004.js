7a7c0061790bd3111b63e004b548ea9415fb8070
LittleCrabXXY <LittleCrabXXY@163.com>
2017-02-19 23:18:15 +0800

    show task

--- ans-task0004/js/task0004.js
+++ ans-task0004/js/task0004.js
@@ -1,11 +1,18 @@
 window.onload = function() {
     initLocalStorage();
     getList('###分类');
+    var list = document.getElementById('list');
+    delegateEvent(list, 'li', 'touchend', function(event) {
+        var target = event.target || event.srcElement;
+        getList(target.innerText);
+    });
 };
 
 window.onunload = function() {
     localStorage.clear();
 };
+
+var count = -1;
 
 function initLocalStorage() {
     localStorage.clear();
@@ -27,6 +34,8 @@ function initLocalStorage() {
 
 function getList(key) {
     var list = document.getElementById('list');
+    list.innerHTML = '';
+    count++;
     if (localStorage.getItem(key)) {
         var cate = localStorage.getItem(key).split(',');
         for (var i=0; i<cate.length; i++) {
@@ -34,5 +43,48 @@ function getList(key) {
             li.innerText = cate[i];
             list.appendChild(li);
         }
+    } else if (localStorage.getItem('@' + key)) {
+        var task = JSON.parse(localStorage.getItem('@' + key));
+        var taskKeys = Object.keys(task);
+        for (i=0; i<taskKeys.length; i++) {
+            var div = document.createElement('div');
+            div.innerText = task[taskKeys[i]];
+            if (taskKeys[i] === 'content') {
+                addId(div, 'content');
+            }
+            list.appendChild(div);
+        }
     }
+    if (count > 0) {
+        var backBtn = document.getElementById('back');
+        setVisibility(backBtn, 'visible');
+    }
+}
+
+// utils
+function setVisibility(element, value) {
+    element.style.visibility = value;
+}
+
+function addId(element, id) {
+    element.id = id;
+}
+
+function addEvent(element, event, listener) {
+    if (element.addEventListener) {
+        element.addEventListener(event, listener);
+    } else if (element.attachEvent) {
+        element.attachEvent('on' + event, listener);
+    } else {
+        element['on' + event] = listener;
+    }
+}
+
+function delegateEvent(element, tag, event, listener) {
+    addEvent(element, event, function(e) {
+        var target = e.target || e.srcElement;
+        if (target.tagName.toLowerCase() === tag) {
+            listener(e);
+        }
+    });
 }
